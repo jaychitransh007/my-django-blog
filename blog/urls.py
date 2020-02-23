cac75684036735acbfafabc603a0091dc5195aaf
jachitransh007 <mj.nigam28@gmail.com>
2020-02-23 16:58:26 +0530

    Extended Application

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 # As you can see, we're now assigning a view called post_list to the root URL
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # The dot before models means current directory or current application
 from .models import Post
 from django.utils import timezone
@@ -11,3 +11,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # The last parameter, {}, is a place in which we can add some things for the template to use.
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
